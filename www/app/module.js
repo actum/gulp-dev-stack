f0008526e5891f1ffece98218aa29d4920a1124d
Jan Panschab <janpanschab@Jan-MacBook-Air.local>
2015-09-15 17:35:37 +0200

    initial commit

---
+++ www/app/module.js
@@ -0,0 +1,10 @@
+/**
+ * @param {jQuery} container
+ */
+var MyModule = function(container) {
+    this.container = container;
+
+    document.querySelector(".main h1").innerText += '_js';
+};
+
+module.exports = MyModule;
