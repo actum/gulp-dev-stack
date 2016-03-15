d8d351311b46039da7e0e7fab7aacb7a5ca5c9b2
Vojta Bulant <vojtech@bulant.cz>
2016-03-15 14:33:25 +0100

    use clean task for both dev and prod pipeline, add unbuild task, update docs – close #28

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -1,7 +1,25 @@
 import gulp from 'gulp';
+import { argv } from 'yargs';
 import del from 'del';
 import config from '../config';
 
-const { dist } = config.paths;
+const {
+    src: { less, app, icon, html },
+    dist
+} = config.paths;
+const srcDest = [less.dest, app.dest, icon.dest, html];
+const distDest = dist.base;
+const isDev = argv.dev || false;
 
-gulp.task('clean', () => del(dist.base));
+/**
+ * Removes either /dist or all built targets in /src
+ * Based on --dev param
+ * Used in `prepare` pipeline
+ */
+gulp.task('clean', () => del(isDev ? srcDest : distDest));
+
+/**
+ * Removes /dist and all built targets in /src
+ * Use standalone as `gulp unbuild`
+ */
+gulp.task('unbuild', () => del([...srcDest, distDest]));
--- gulp/tasks/prepare.js
+++ gulp/tasks/prepare.js
@@ -2,13 +2,10 @@ import gulp from 'gulp';
 import { argv } from 'yargs';
 import runSequence from 'run-sequence';
 
-// lets use 'prepare' task for both dev and prod so we can use production build sequence for both build task and prod serve task
-// before this change, we've been using dev 'prepare' in prod serve task…
-
 // TODO run browserSync after all tasks finished
 const isDev = argv.dev || false;
-const devSequence = ['icon', ['less', 'js', 'tpl']];
-const buildSequence = ['clean', ...devSequence];
+const devSequence = ['clean', 'icon', ['less', 'js', 'tpl']];
+const buildSequence = devSequence;
 const sequence = isDev ? devSequence : buildSequence;
 
 gulp.task('prepare', () => runSequence(...sequence));
