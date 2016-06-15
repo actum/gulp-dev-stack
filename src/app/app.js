087edaed1b7ce9da77858265b432d6c2cef9a84c
Vojta Bulant <vojtech@bulant.cz>
2016-06-15 16:57:57 +0200

    add babel-polyfill, add svgxuse polyfill for async svg fetch – fixes #39

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,9 @@
+import 'babel-polyfill';
+/**
+ * https://github.com/Keyamoon/svgxuse
+ * If you do not use SVG <use xlink:href="…"> elements, remove svgxuse module
+ */
+import 'svgxuse';
 import init from './init';
 import factory from './factory';
 import MyModule from './components/module';
