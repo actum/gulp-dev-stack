c08ea6a085e4bf9ee670232b415e82a8391ebe23
Jan Panschab <jan.panschab@gmail.com>
2016-09-06 07:50:06 +0200

    Stylelint (#49)
    * add stylelint for css
    
    * stylelint in dev mode

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -3,6 +3,7 @@ import { argv } from 'yargs';
 import gulpif from 'gulp-if';
 import eslint from 'gulp-eslint';
 import config from '../config';
+const gulpStylelint = require('gulp-stylelint');
 
 const { gulpfile, src } = config.paths;
 const isDev = argv.dev || false;
@@ -23,4 +24,16 @@ const lint = (globs) => {
 };
 gulp.task('lint:app', () => lint(src.app.all));
 gulp.task('lint:gulpfile', () => lint(gulpfile));
-gulp.task('lint', ['lint:gulpfile', 'lint:app']);
+
+gulp.task('lint:styles', () => {
+    return gulp
+        .src(src.styles.all)
+        .pipe(gulpStylelint({
+            failAfterError: false,
+            reporters: [{
+                formatter: 'string',
+                console: true
+            }]
+        }));
+});
+gulp.task('lint', ['lint:gulpfile', 'lint:app', 'lint:styles']);
