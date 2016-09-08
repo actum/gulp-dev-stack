379594a438360fcf85541cd0e7a16093b034e58d
Artem Zakharchenko <artem.zakharchenko@actum.cz>
2016-09-08 10:48:49 +0200

    Remove lint.js different rules on different environments.

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -10,42 +10,9 @@ const gulpfile = config.paths.gulpfile;
 const src = config.paths.src;
 const isDev = argv.dev || false;
 
-/* Rule set for different environments */
-const devRules = {
-    /* Visuals */
-    "semi": 2,
-    "indent": [2, 4],
-    "max-len": 0,
-    "no-multiple-empty-lines": [2, { "max": 1}],
-    "space-before-function-paren": 0,
-    "padded-blocks": 0,
-
-    "quotes": [2, "single"],
-    "strict": [2, "never"],
-
-    "one-var": [2, "never"],
-
-    "no-alert": 0,
-    "no-console": 0,
-    "no-tabs": 2,
-    "no-param-reassign": [2, { "props": false }],
-    "no-unused-vars": 0,
-
-    /* Import */
-    "import/imports-first": 2,
-    "import/newline-after-import": 1,
-    "import/no-extraneous-dependencies": 1,
-
-    "no-new-object": 2,
-    "object-shorthand": 2,
-    "quote-props": 2,
-}
-
 const lint = (globs) => {
-    const rules = isDev ? devRules : {};
-
     return gulp.src(globs)
-        .pipe(eslint({ rules }))
+        .pipe(eslint())
         .pipe(eslint.format())
         .pipe(gulpif(!isDev, eslint.failOnError()));
 };
@@ -63,4 +30,5 @@ gulp.task('lint:styles', () => {
             }]
         }));
 });
+
 gulp.task('lint', ['lint:gulpfile', 'lint:app', 'lint:styles']);
