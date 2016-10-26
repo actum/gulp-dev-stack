fda83837f838924d886a60d4a6efcb92ca3a8bf9
Artem Zakharchenko <artem.zakharchenko@actum.cz>
2016-10-26 15:30:40 +0200

    Gulp: ESLint for Gulp files

---
+++ gulp/tasks/eslint.js
@@ -0,0 +1,28 @@
+/* Environment */
+const config = require('../config');
+const DEVELOPMENT = config.environment.isDevelopment;
+const PRODUCTION = !DEVELOPMENT;
+
+/* Gulp */
+const gulp = require('gulp');
+const gulpif = require('gulp-if');
+const eslint = require('gulp-eslint');
+
+const lint = (globs) => {
+    const opts = DEVELOPMENT ? {
+        'rules': {
+            'no-empty': 0,
+            'space-in-parens': 0,
+            'no-unused-vars': 0,
+            'no-multiple-empty-lines': 0
+        }
+    } : {};
+    return gulp.src(globs)
+        .pipe(eslint(opts))
+        .pipe(eslint.format())
+        .pipe(gulpif(PRODUCTION, eslint.failOnError()));
+};
+
+gulp.task('eslint:gulpfile', () => lint(config.GULP_ALL));
+
+gulp.task('eslint', ['eslint:gulpfile']);
