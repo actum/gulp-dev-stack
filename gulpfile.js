26f03b6f38bde8301b1686c9d617a3d5c782ef7f
Jan Panschab <jan.panschab@actum.cz>
2016-11-24 20:15:11 +0100

    minimal env version check

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,18 @@
+// check the minimal supported version of node
+const pkg = require('./package.json');
+const gutil = require('gulp-util');
+
+const nodeMajorVersion = process.version.substr(1, 1);
+const minMajorVersion = pkg.engines.node.substr(-1);
+
+if (nodeMajorVersion < minMajorVersion) {
+    gutil.log(
+        gutil.colors.red(`Node version ${pkg.engines.node} is required.`),
+        gutil.colors.cyan(`You are using ${process.version}`)
+    );
+    process.exit(1);
+}
+
 const config = require('./gulp/config');
 process.env.NODE_ENV = config.environment.type;
 const gulp = require('gulp');
