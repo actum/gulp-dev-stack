28f2d98320d5f0b9bdf06f7fc9aedab1bc67baf2
Jan Panschab <jan.panschab@actum.cz>
2016-11-25 16:05:18 +0100

    pkg > package

--- gulp/environment.js
+++ gulp/environment.js
@@ -1,6 +1,6 @@
 const argv = require('yargs').argv;
 const isDevelopment = argv.dev || false;
-const pkg = require('../package.json');
+const package = require('../package.json');
 const gutil = require('gulp-util');
 
 const DEVELOPMENT = 'DEVELOPMENT';
@@ -9,11 +9,11 @@ const PRODUCTION = 'PRODUCTION';
 // check the minimal supported version of node
 function check() {
     const nodeMajorVersion = process.version.substr(1, 1);
-    const minMajorVersion = pkg.engines.node.substr(-1);
+    const minMajorVersion = package.engines.node.substr(-1);
 
     if (nodeMajorVersion < minMajorVersion) {
         gutil.log(
-            gutil.colors.red(`Node version ${pkg.engines.node} is required.`),
+            gutil.colors.red(`Node version ${package.engines.node} is required.`),
             gutil.colors.cyan(`You are using ${process.version}`)
         );
         process.exit(1);
