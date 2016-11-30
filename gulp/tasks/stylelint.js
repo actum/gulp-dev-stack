f463f72c8237bfc9f8dd3637d1516e1713eaf4fd
Andrei Fidelman <andrei.fidelman@actum.cz>
2016-11-30 19:12:15 +0100

    add cache to the styleling task

--- gulp/tasks/stylelint.js
+++ gulp/tasks/stylelint.js
@@ -1,12 +1,16 @@
 const gulp = require('gulp');
 const stylelint = require('gulp-stylelint');
 const config = require('../config');
+const cached = require('gulp-cached');
+const debug = require('gulp-debug');
 
 const src = config.paths.src;
 
 gulp.task('stylelint', () => {
     return gulp
         .src(src.styles.all)
+        .pipe(cached('stylelint'))
+        .pipe(debug())
         .pipe(stylelint({
             failAfterError: false,
             reporters: [{
