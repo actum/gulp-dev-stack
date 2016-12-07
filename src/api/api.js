b3c025fdd4e4388407b714552fdd21199c8a33e5
Vojta Bulant <vojtech@bulant.cz>
2016-12-07 15:31:54 +0100

    remove dummy json, add api structure example

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,14 @@
-module.exports = () => ({
-    gulp: {
-        running: true
-    }
-});
+module.exports = () => {
+    const usersCount = 10;
+    const createUser = (val, i) => ({
+        id: i,
+        name: `${val}_${i}`
+    });
+    const data = {
+        users: Array(usersCount)
+            .fill('user')
+            .map(createUser)
+    };
+
+    return data;
+};
