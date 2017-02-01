d664bd297f503602c6ba512f2f36beeb14e2470b
Artem Zakharchenko <kettanaito@gmail.com>
2017-02-01 17:11:09 +0100

    Stylelint now fails after error in Production mode (#131)

--- gulp/tasks/stylelint.js
+++ gulp/tasks/stylelint.js
@@ -1,4 +1,5 @@
 const config = require('../config');
+const isProduction = config.environment.isProduction;
 const gulp = require('gulp');
 const stylelint = require('gulp-stylelint');
 const cached = require('gulp-cached');
@@ -8,7 +9,7 @@ gulp.task('stylelint', () => {
         .src(config.CSS_ALL)
         .pipe(cached('stylelint'))
         .pipe(stylelint({
-            failAfterError: false,
+            failAfterError: isProduction,
             reporters: [{
                 formatter: 'string',
                 console: true
