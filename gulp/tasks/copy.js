d880cb1a03adb7faa3d2505eed74c1c1e3a408fc
Ronald Ružička <ronald.ruzicka@actum.cz>
2017-02-16 10:22:40 +0100

    live reload styles in styleguide

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,10 +1,12 @@
 const config = require('../config');
 const gulp = require('gulp');
+const browserSync = require('browser-sync');
 const merge = require('merge-stream');
 
 gulp.task('copySgAssets', () => {
     const css = gulp.src(`${config.CSS_BUILD}/*`)
-        .pipe(gulp.dest(`${config.STYLEGUIDE_DEST}/css`));
+        .pipe(gulp.dest(`${config.STYLEGUIDE_DEST}/css`))
+        .pipe(browserSync.stream());
 
     const js = gulp.src(`${config.JS_BUILD}/*`)
         .pipe(gulp.dest(`${config.STYLEGUIDE_DEST}/js`));
