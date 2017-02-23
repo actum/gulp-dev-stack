fed53a30ec1071d79b76becf333d8db54a68acb9
Ronald Ružička <ronald.ruzicka@actum.cz>
2017-02-23 14:28:18 +0100

    reload css for styleguide only in DEV mode

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,12 +1,14 @@
 const config = require('../config');
+const DEVELOPMENT = require('../environment').isDevelopment;
 const gulp = require('gulp');
+const gulpif = require('gulp-if');
 const browserSync = require('browser-sync');
 const merge = require('merge-stream');
 
 gulp.task('copySgAssets', () => {
     const css = gulp.src(`${config.CSS_BUILD}/*`)
         .pipe(gulp.dest(`${config.STYLEGUIDE_DEST}/css`))
-        .pipe(browserSync.stream());
+        .pipe(gulpif(DEVELOPMENT, browserSync.stream()));
 
     const js = gulp.src(`${config.JS_BUILD}/*`)
         .pipe(gulp.dest(`${config.STYLEGUIDE_DEST}/js`));
