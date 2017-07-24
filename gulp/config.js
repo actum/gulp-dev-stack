9a2ecd20d49e21e5790b2537f6c2214af79ac769
Jiří <zmrhal.j@gmail.com>
2017-07-24 11:42:59 +0200

    Update config.js - minor fix
    Replace semicolon with comma in a sequence of defining variables.

--- gulp/config.js
+++ gulp/config.js
@@ -23,7 +23,7 @@ const
 
     /* JavaScript */
     JS_BASE = `${DEVELOPMENT_BASE}/app`,
-    JS_MAIN_FILENAME = 'app.js';
+    JS_MAIN_FILENAME = 'app.js',
     JS_ENTRY = `${JS_BASE}/${JS_MAIN_FILENAME}`,
     JS_ALL = `${JS_BASE}/**/*.js`,
     JS_VENDOR_ALL = `${JS_BASE}/vendor/**/*.js`,
