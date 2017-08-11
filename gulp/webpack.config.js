8c3c6f2c45722e02c5aa51708344d8c92ce6e7b4
Ronald Ruzicka <I>
2017-08-11 15:20:57 +0200

    wip - return gulp-js task with browserify for now

--- gulp/webpack.config.js
+++ gulp/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         ]
     },
     resolve: {
-        modules: ["node_modules"],
+        modules: ['node_modules']
     },
     plugins: [
         new webpack.NoEmitOnErrorsPlugin()
