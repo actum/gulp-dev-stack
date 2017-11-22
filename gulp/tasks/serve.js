5f32a6ef6eec7626de1b5c64a1f6b1b1c2f71b2c
Ronald Ružička <ronald.ruzicka@actum.cz>
2017-11-22 10:15:41 +0100

    Remove styleguide folder from browserSync baseDir

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -11,8 +11,7 @@ const port = config.PORT;
 gulp.task('serve', ['prepare'], () => {
     const baseDir = DEVELOPMENT ? [
         config.BUILD_BASE,
-        config.NPM,
-        config.STYLEGUIDE_BASE
+        config.NPM
 
     ] : config.BUILD_BASE;
 
