1f354db4f7c96980fc12cc23da66bb8c8aae6db5
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-03-10 18:51:34 +0100

    add gulp task `gulp server`

---
+++ gulp/tasks/server.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+
+const config = require('../config');
+
+function server() {
+    nodemon({
+        script: config.MOCK_START,
+        watch: config.MOCK_BASE
+    });
+}
+
+gulp.task('server', server);
