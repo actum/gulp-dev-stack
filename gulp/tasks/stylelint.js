9a57ce89519a8e670a197a8bc1bd42831a2f2a9c
Ondrej Mladek <ondrej.mladek@actum.cz>
2018-08-27 21:22:18 +0200

    add automatic sorting of CSS properties according to our convention

--- gulp/tasks/stylelint.js
+++ gulp/tasks/stylelint.js
@@ -1,20 +1,39 @@
+const fs = require('fs');
+
 const gulp = require('gulp');
+const gulpif = require('gulp-if');
 const stylelint = require('gulp-stylelint');
 const cached = require('gulp-cached');
+const postcss = require('gulp-postcss');
+const postscss = require('postcss-scss');
+const postcssSorting = require('postcss-sorting');
 
 const config = require('../config');
 
-const PRODUCTION = config.environment.isProduction;
+const stylelintrc = JSON.parse(fs.readFileSync('.stylelintrc', 'utf8'));
 
-gulp.task('stylelint', () => {
-    return gulp
-        .src(config.CSS_ALL)
-        .pipe(cached('stylelint'))
-        .pipe(stylelint({
-            failAfterError: PRODUCTION,
-            reporters: [{
-                formatter: 'string',
-                console: true
-            }]
-        }));
-});
+const PRODUCTION = config.environment.isProduction;
+const DEVELOPMENT = config.environment.isDevelopment;
+const propertiesOrder = stylelintrc.rules['order/properties-order'];
+
+gulp.task('stylelint', () => gulp
+  .src(config.CSS_ALL)
+  .pipe(cached('stylelint'))
+  .pipe(gulpif(DEVELOPMENT,
+    postcss(
+      [
+        postcssSorting({ 'properties-order': propertiesOrder })
+      ],
+      { syntax: postscss }
+    )
+  ))
+  .pipe(gulpif(DEVELOPMENT, gulp.dest(config.CSS_BASE)))
+  .pipe(stylelint({
+    failAfterError: PRODUCTION,
+    reporters: [{
+      formatter: 'string',
+      console: true,
+    }],
+    fix: DEVELOPMENT,
+  }))
+  .pipe(gulpif(DEVELOPMENT, gulp.dest(config.CSS_BASE))));
